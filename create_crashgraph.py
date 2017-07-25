a2a0ecfb1e48efda2dcc5c94a89daed0688881cb
Domagoj Stolfa <ds815@cam.ac.uk>
2017-07-25 14:23:13 +0100

    Document the file so that it's easier to follow and extend.

--- create_crashgraph.py
+++ create_crashgraph.py
@@ -34,43 +34,67 @@ def disassemble(instructions):
         print i
 
 if __name__ == '__main__':
+    # Create the debugger
     debugger = lldb.SBDebugger.Create()
     debugger.SetAsync(False)
 
+    # FIXME: We currently set our target as a.out for testing purposes, this
+    # should however be absolutely arbitrary (parhaps a path to the input test
+    # cases and binary?
     target = debugger.CreateTarget('./a.out')
 
     if target:
+        # Launch the traget in the current working directory
         process = target.LaunchSimple(None, None, os.getcwd())
         if process:
             state = process.GetState()
             print process
-            if state == lldb.eStateCrashed or state == lldb.eStateStopped:
+            # XXX: Currently, we will only check for eStateStopped, as it is
+            # unclear to me what eStateCrashed means.
+            if state == lldb.eStateStopped:
+
+                # FIXME: Assumption that we are running on one thread (not
+                # always true!)
                 thread = process.GetThreadAtIndex(0)
 
                 if thread:
                     print thread
 
                     stop_reason = thread.GetStopReason()
+                    # If the stopping reason is not a signal, we just continue
+                    # execution
                     if stop_reason != lldb.eStopReasonSignal:
                         process.Continue()
 
                     sig = thread.GetStopReasonDataAtIndex(1)
+                    # Check for SIGSEGV and SIGABRT
+                    # XXX: This should probably be written in a more generic
+                    # sense?
                     if sig != signal.SIGSEGV or signal.SIGABRT:
                         process.Continue()
+
+                    # Get the frame where we stopped
+                    # FIXME: We should walk all of the frames here and down to
+                    # the interested one. We are creating a graph, not just a
+                    # histogram
                     frame = thread.GetFrameAtIndex(0)
                     if frame:
+                        # Get the function that we crashed in
                         function = frame.GetFunction()
                         if function:
                             print function
                             instructions = function.GetInstructions(target)
                             disassemble(instructions)
                         else:
+                            # It wasn't a function, attempt the same thing with
+                            # a symbol
                             symbol = frame.GetSymbol()
                             if symbol:
                                 print symbol
                                 instructions = symbol.GetInstructions(target)
                                 disassemble(instructions)
 
+                        # Walk the register list
                         registerList = frame.GetRegisters()
                         print 'Frame registers (size of register set = {}):'.format(registerList.GetSize())
                         for value in registerList:
@@ -79,8 +103,9 @@ if __name__ == '__main__':
                                 print 'Name: {} Value: {}'.format(child.GetName(), child.GetValue())
 
                     process.Continue()
+            # If we have successfully exited, there should be no crash graph.
             elif state == lldb.eStateExited:
-                print "Didn't hit the BP"
+                print "There have been no crashes in the executable"
             else:
                 print 'Unexpected process state: {}'.format(debugger.StateAsCString(state))
                 process.Kill()
