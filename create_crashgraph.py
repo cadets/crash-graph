4b43d3c2523f9ca6955286e33146964776dedc7d
Domagoj Stolfa <ds815@cam.ac.uk>
2017-07-26 14:28:19 +0100

    Allow it to run multiple test cases.

--- create_crashgraph.py
+++ create_crashgraph.py
@@ -93,13 +93,18 @@ class CGCrash:
 class CGDebugger:
     def __init__(self,
                  binary_path='./a.out',
-                 indata='out',
+                 inpath='out',
                  sigstocatch=(signal.SIGSEGV, signal.SIGABRT)):
         # We hook into lldb here
         self.debugger = lldb.SBDebugger.Create()
         self.debugger.SetAsync(False)
 
-        self.indata = indata
+        self.inpath = inpath
+        self.test_cases = []
+
+        for root, dirs, files in os.walk(self.inpath):
+            for fname in files:
+                self.test_cases.append(os.path.join(root, fname))
 
         # Create the debugging target and identify which signals we want to
         # catch
@@ -111,64 +116,74 @@ class CGDebugger:
     def run(self):
         if not self.target:
             return
-        # Launch the process
-        process = self.target.LaunchSimple(None, None, os.getcwd())
-        cgcrash = CGCrash()
-
-        if not process:
-            return
-        print process
-
-        state = process.GetState()
-        if state == lldb.eStateExited:
-            print 'No crashes observed in the process'
-        elif state != lldb.eStateStopped:
-            print 'Unexpected process state: {}'.format(self.debugger.StateAsCString(state))
-            process.Kill()
-        else:
-            thread = process.GetThreadAtIndex(0)
-            if not thread:
+        
+        # Iterate over the test cases
+        for tc in self.test_cases:
+            error = lldb.SBError()
+            process = self.target.Launch(self.debugger.GetListener(),
+                                         None,
+                                         None,
+                                         tc,
+                                         None,
+                                         None,
+                                         os.getcwd(),
+                                         0,
+                                         False,
+                                         error)
+            if not process:
                 return
-            cgcrash.thread = thread
-            print thread
-
-            # We only want to examine if we got a signal, not any
-            # other condition.
-            stop_reason = thread.GetStopReason()
-            if stop_reason != lldb.eStopReasonSignal:
-                process.Continue()
-            sig = thread.GetStopReasonDataAtIndex(1)
-            if sig not in self.sigstocatch:
-                process.Continue()
-
             cgcrash = CGCrash()
-            for frame in thread:
-                if not frame:
-                    continue
-                func = frame.GetFunction()
-                if not func:
-                    continue
-                fargs = frame.GetVariables(True,
-                                           False,
-                                           False,
-                                           False)
-                cgfunction = CGFunction(function_type=func.GetType(),
-                                        name=func.GetName())
-                for arg in fargs:
-                    cgfunction.add_arg(arg.GetTypeName(),
-                                       arg.GetName(),
-                                       arg.GetValue())
-                cgframe = CGFrame(cgfunction,
-                                  frame.GetRegisters(),
-                                  frame.GetLineEntry())
-                cgcrash.add_frame(cgframe)
-            self.crashes.append(cgcrash)
-            process.Kill()
-            return
-        process.Continue()
+
+            state = process.GetState()
+            if state == lldb.eStateExited:
+                print 'No crashes observed in the process'
+            elif state != lldb.eStateStopped:
+                print 'Unexpected process state: {}'.format(self.debugger.StateAsCString(state))
+                process.Kill()
+            else:
+                thread = process.GetThreadAtIndex(0)
+                if not thread:
+                    return
+                cgcrash.thread = thread
+
+                # We only want to examine if we got a signal, not any
+                # other condition.
+                stop_reason = thread.GetStopReason()
+                if stop_reason != lldb.eStopReasonSignal:
+                    process.Continue()
+                sig = thread.GetStopReasonDataAtIndex(1)
+                if sig not in self.sigstocatch:
+                    process.Continue()
+
+                for frame in thread:
+                    if not frame:
+                        continue
+                    func = frame.GetFunction()
+                    if not func:
+                        continue
+                    fargs = frame.GetVariables(True,
+                                               False,
+                                               False,
+                                               False)
+                    cgfunction = CGFunction(function_type=func.GetType(),
+                                            name=func.GetName())
+                    for arg in fargs:
+                        cgfunction.add_arg(arg.GetTypeName(),
+                                           arg.GetName(),
+                                           arg.GetValue())
+                    cgframe = CGFrame(cgfunction,
+                                      frame.GetRegisters(),
+                                      frame.GetLineEntry())
+                    cgcrash.add_frame(cgframe)
+                self.crashes.append(cgcrash)
+                process.Kill()
+                continue
+            process.Continue()
 
     def stdout_dump(self):
         for crash in self.crashes:
+            print crash.thread.GetProcess()
+            print crash.thread
             for frame in crash.get_backtrace():
                 cgfunc = frame.function
                 arg_str = ", ".join(["{} {} = {}".format(arg.atype,
